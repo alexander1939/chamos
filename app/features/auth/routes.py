4688fae57f3201b85ef69bd38a7e7613894e718d
GaelZ23 <gaelespfer128@gmail.com>
2025-01-23 12:09:16 -0600

    Importacion abort en routes

--- app/features/auth/routes.py
+++ app/features/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, abort, render_template, redirect, url_for, flash, request
 from flask_login import login_user
 from flask_login import login_required, logout_user,  current_user
 from werkzeug.security import generate_password_hash, check_password_hash
